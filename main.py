3dcaa329da3f844f9d0b1adabb57856f6160c0cb
wishalta <rolandas.lebed@gmail.com>
2024-11-22 10:52:31 +0200

    Cookies done

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from Barbora import Barbora_Scrapper
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 
-# From zero to hero
+
+
+#Reikia su selenium pasiimti duomenys, is barboros
+#Veliau siuos duomenys issiusti i mysql
+
+
+
+def initGathering():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    wait = WebDriverWait(driver, 10)
+    return driver, wait
+
+def executeDataGathering():
+    driver, wait = initGathering()
+    barbora = Barbora_Scrapper(driver, 'https://www.barbora.lt/')
+    barbora.Activiation()
+
+executeDataGathering()
